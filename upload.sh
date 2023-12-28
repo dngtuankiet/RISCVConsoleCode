b9fbfabf90cb4576d1f0e81de1ea977f96458eea
dngtuankiet <dngtuankiet97@gmail.com>
2023-12-28 11:59:02 +0900

    proper check upload

--- upload.sh
+++ upload.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-sudo dd if=./build/out.bin of=/dev/sde1 conv=fsync bs=4096
+echo Upload to SD card
+
+read -p "Want to check USB first (y/n)? " readUSB
+
+if [[ $readUSB == "y" || $readSUB == "Y" ]]; then
+lsblk
+fi
+
+read -p "Type in device (e.g /dev/sd?1): " device
+
+read -p "Proceed (y/n)? " check
+
+if [[ $check == "y" || $check == "Y" ]]; then
+sudo dd if=./build/out.bin of=$device conv=fsync bs=4096
+else
+echo "Abort upload"
+fi
